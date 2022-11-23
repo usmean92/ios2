58f9ebd6c7090ac7d283439cbfdf8133e5517be6
Nabiha Zubair <nabiha.zubair@devsinc.com>
2022-11-23 12:14:38 +0500

    Master: User Auth

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import './service/db.js'
+
+const PORT = process.env.PORT || 8000;
+import UserRoutes from './routes/user.js'
+
+const app = express()
+app.use(cors())
+dotenv.config()
+app.use(bodyParser.json({ limit: "20mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }))
+app.use('/user', UserRoutes)
+app.listen(PORT, function (error) {
+  if (error) throw error
+  console.log("Server started successfully on port", PORT)
+})
